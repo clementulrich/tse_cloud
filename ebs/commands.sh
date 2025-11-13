149be198c6d9fa58a178eaeadfb52bc4689bfa2e
Clément ULRICH <clement.ulrich79@gmail.com>
2025-11-13 10:33:43 +0100

    Ajout commentaires cmd EC2 volumes

--- ebs/commands.sh
+++ ebs/commands.sh
@@ -4,9 +4,9 @@ sudo mkfs -t ext4 /dev/xvdf
 
 sudo mkdir /mnt/volume/
 
-sudo mount /dev/xvdf /mnt/volume/
+sudo mount /dev/nvme1n1 /mnt/volume/            # attention modif xvdf en nvme1n1 !!!
 
-df -hk
+df -hk                                          # on voit bien apparaitre le /mnt/volume/
 
 sudo touch /mnt/volume/testfile
 
@@ -18,5 +18,5 @@ sudo umount /mnt/volume/
 
 sudo mount /dev/xvdf /mnt/volume/
 
-ls /mnt/volume
+ls /mnt/volume                                  # On voit bien le testfile apparaitre alléluia !!!
 
